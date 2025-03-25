56ff047463669861ce0149b8867d72d5f46ebc35
안혜연 <ahy020606@naver.com>
2025-03-26 01:58:47 +0900

    [feature] axios 요청 인터셉터 훅 useSetInterceptors.ts #11

--- src/api/customAxios.ts
+++ src/api/customAxios.ts
@@ -1,11 +1,10 @@
 import axios, { AxiosInstance } from "axios";
-import { getCookie } from "./cookie";
 
 export const customAxios: AxiosInstance = axios.create({
   baseURL: `${import.meta.env.VITE_APP_BASE_URL}`,
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
-    accessToken: await getCookie("accessToken"),
   },
+  withCredentials: true,
 });
