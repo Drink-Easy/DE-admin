34f3eb5bf08430428d584119aa0a45e0ac1b3e02
안혜연 <ahy020606@naver.com>
2025-03-26 02:14:35 +0900

    [feature] router.tsx로 라우터 설정 #11

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,23 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
 import { ThemeProvider } from "styled-components";
 import { CookiesProvider } from "react-cookie";
 import { QueryClient, QueryClientProvider } from "react-query";
 import GlobalStyle from "./styles/GlobalStyle";
 import theme from "./styles/theme";
-import MainPage from "./pages/home/MainPage";
-import LoginPage from "./pages/login/LoginPage";
-import ClassPage from "./pages/class/ClassPage";
-import ClassMemberPage from "./pages/class/ClassMemberPage";
-import WinePage from "./pages/wine/WinePage";
-import AddWinePage from "./pages/wine/AddWinePage";
-import UserPage from "./pages/user/UserPage";
-import ReportPage from "./pages/report/ReportPage";
-import NoshowReportPage from "./pages/report/NoshowReportPage";
-import CompletedReportPage from "./pages/report/CompletedReportPage";
-import WineDetailPage from "./pages/wine/WineDetailPage";
-import UserDetailPage from "./pages/user/UserDetailPage";
+import useSetInterceptors from "./hooks/useSetInterceptors";
 
 const queryClient = new QueryClient();
 
 function App() {
+  useSetInterceptors();
+
   return (
     <CookiesProvider>
       <QueryClientProvider client={queryClient}>
         <ThemeProvider theme={theme}>
+          <RouterProvider router={router} />
           <GlobalStyle />
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<MainPage />}></Route>
-              <Route path="/login" element={<LoginPage />}></Route>
-
-              <Route path="/class" element={<ClassPage />}></Route>
-              <Route path="/class/member" element={<ClassMemberPage />}></Route>
-
-              <Route path="/wine" element={<WinePage />}></Route>
-              <Route path="/wine/:id" element={<WineDetailPage />}></Route>
-              <Route path="/wine/add" element={<AddWinePage />}></Route>
-
-              <Route path="/user" element={<UserPage />}></Route>
-              <Route path="/user/:id" element={<UserDetailPage />}></Route>
-
-              <Route path="/report" element={<ReportPage />}></Route>
-              <Route
-                path="/report/noshow"
-                element={<NoshowReportPage />}
-              ></Route>
-              <Route
-                path="/report/completed"
-                element={<CompletedReportPage />}
-              ></Route>
-            </Routes>
-          </BrowserRouter>
         </ThemeProvider>
       </QueryClientProvider>
     </CookiesProvider>
---
+++ src/router.tsx
@@ -0,0 +1,54 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainPage from "./pages/home/MainPage";
+import LoginPage from "./pages/login/LoginPage";
+import ClassPage from "./pages/class/ClassPage";
+import ClassMemberPage from "./pages/class/ClassMemberPage";
+import WinePage from "./pages/wine/WinePage";
+import AddWinePage from "./pages/wine/AddWinePage";
+import UserPage from "./pages/user/UserPage";
+import ReportPage from "./pages/report/ReportPage";
+import NoshowReportPage from "./pages/report/NoshowReportPage";
+import CompletedReportPage from "./pages/report/CompletedReportPage";
+import WineDetailPage from "./pages/wine/WineDetailPage";
+import UserDetailPage from "./pages/user/UserDetailPage";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/class",
+    element: <ClassPage />,
+    children: [
+      { path: "member", element: <ClassMemberPage /> }, // /class/member
+    ],
+  },
+  {
+    path: "/wine",
+    element: <WinePage />,
+    children: [
+      { path: "add", element: <AddWinePage /> }, // /wine/add
+      { path: ":id", element: <WineDetailPage /> }, // /wine/:id
+    ],
+  },
+  {
+    path: "/user",
+    element: <UserPage />,
+    children: [
+      { path: ":id", element: <UserDetailPage /> }, // /user/:id
+    ],
+  },
+  {
+    path: "/report",
+    element: <ReportPage />,
+    children: [
+      { path: "noshow", element: <NoshowReportPage /> }, // /report/noshow
+      { path: "completed", element: <CompletedReportPage /> }, // /report/completed
+    ],
+  },
+]);
