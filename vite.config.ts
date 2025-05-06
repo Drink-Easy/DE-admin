1f4cda8b41f349373aac46f711c823d4ecfd2491
안혜연 <ahy020606@naver.com>
2025-05-06 23:45:28 +0900

    [fix] 버셀 배포 오류 해결 #18

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import fs from "fs";
-import path from "path";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -10,12 +8,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     https: {
-      key: fs.readFileSync(
-        path.resolve(__dirname, process.env.PEM_KEY_PATH || "ssl/key.pem")
-      ),
-      cert: fs.readFileSync(
-        path.resolve(__dirname, process.env.PEM_CERT_PATH || "ssl/cert.pem")
-      ),
+      key: (process.env.SSL_KEY || "").replace(/\\n/g, "\n"),
+      cert: (process.env.SSL_CERT || "").replace(/\\n/g, "\n"),
     },
     port: 5173,
   },
